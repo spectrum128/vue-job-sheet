96049964dd3bb4704dccc44bb65c5dad5e87bdbb
unknown <declan@softdev.io>
2020-04-18 15:19:12 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Job Sheet Builder</h1>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Main from './pages/Main.vue'
+import About from './pages/About.vue'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const routes = [
+  { path: '/', component: Main },
+  { path: '/about', component: About }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
